72192d4bfab6e99c9aea068379673a39ceeb941d
behappypro <39382867+behappypro@users.noreply.github.com>
2022-01-07 02:59:29 +0100

    Added burger menu and mobile css

--- js/main.js
+++ js/main.js
@@ -374,7 +374,7 @@ window.onerror = function(msg, url, linenumber) {
    }
 
    function countDown(){
-    var timeleft = 2;
+    var timeleft = 2000;
     var downloadTimer = setInterval(function(){
       if(timeleft <= 0){
         clearInterval(downloadTimer);
@@ -585,9 +585,44 @@ window.onerror = function(msg, url, linenumber) {
     }
     
   }
-   
 
-      
+  var menu = document.querySelector(".menu")
+  var ham = document.querySelector(".ham")
+  var xIcon = document.querySelector(".xIcon")
+  var menuIcon = document.querySelector(".menuIcon")
+  var body = document.getElementsByTagName("BODY")[0];
+
+  ham.addEventListener("click", toggleMenu)
+
+  function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+     menu.classList.remove("showMenu");
+     xIcon.style.display = "none";
+     menuIcon.style.display = "block";
+     body.style.overflow = "auto";
+  } 
+  else {
+     menu.classList.add("showMenu");
+     
+     xIcon.style.display = "block";
+     menuIcon.style.display = "none";
+     body.style.overflow = "hidden";
+  }
+  }
+
+  var menuLinks = document.querySelectorAll(".menu-item")
+
+  menuLinks.forEach(
+  function (menuLink) {
+     menuLink.addEventListener("click", toggleMenu)
+  }
+  )
+
+ 
+
+
+    
+  
     
  
     
